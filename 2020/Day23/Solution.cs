32b22552602c80ffe928784faee4b6a5b3f2d7d4
Brian Brown <brbrowngeo@gmail.com>
2020-12-23 09:51:41 -0500

    D23 P2

--- 2020/Day23/Solution.cs
+++ 2020/Day23/Solution.cs
@@ -5,66 +5,85 @@ using System.Linq;
 namespace AdventOfCode.Y2020.D23
 {
 
+  internal class Cup
+  {
+      public int Value { get; set; }
+      public Cup Next { get; set; }
+      
+      public Cup(int value)
+      {
+          Value = value;
+      }
+  }
+
   public class Solution: ISolution {
   
     public string Name {get;} = "Crab Cups";
 
-    public long PartOne(string input) => ReadCupsAfterOne(PlayCrabCups(Parse(input), 100));
-    public long PartTwo(string input) => PlayCrabCups(MillionCups(Parse(input)), 10000000).Count();
+    public long PartOne(string input) => ReadCupsAfterOne(PlayCrabCups(Parse(input), 100).ToArray());
+    public long PartTwo(string input) => ReadNextTwoCups(PlayCrabCups(MillionCups(Parse(input)), 10_000_000));
 
     int[] MillionCups(int[] cups) {
-      var newCups = new int[1000000];
-      for (var i = 0; i < 1000000; i++) {
+      var newCups = new int[1_000_000];
+      for (var i = 0; i < 1_000_000; i++) {
         newCups[i] = i < cups.Length ? cups[i] : i + 1;
       }
       return newCups;
     }
 
-    int[] PlayCrabCups(int[] cups, int rounds) {
-      Func<int,int> mod = (int x) => (x%cups.Length + cups.Length)%cups.Length;
-      Func<int, int> nextCup = (int cup) => cup - 1 >= cups.Min() ? cup - 1 : cups.Max();
-      Func<int, int> getCup = (int idx) => cups[mod(idx)];
-      var currentCupIdx = 0;
-      for (var r = 1; r <= rounds; r++) {
-        var destinationCup = nextCup(getCup(currentCupIdx));
-        var pickedUpCups = new int[3]{getCup(currentCupIdx+1), getCup(currentCupIdx+2), getCup(currentCupIdx+3)};
-        // find destination cup
-        while (pickedUpCups.Contains(destinationCup)) {
-          destinationCup = nextCup(destinationCup);
-        }
-        // get all cups between pickedUp cups and destination
-        var nextCups = new Queue<int>();
-        var readIdx = currentCupIdx+4;
-        while (!nextCups.Contains(destinationCup)) {
-          nextCups.Enqueue(getCup(readIdx));
-          readIdx++;
-        }
-        foreach(var cup in pickedUpCups) {
-          nextCups.Enqueue(cup);
-        }
-        // move cups around
-        var writeIdx = currentCupIdx+1; 
-        while (nextCups.Any()) {
-          var cup = nextCups.Dequeue();
-          cups[mod(writeIdx)] = cup;
-          writeIdx++;
-        }
-        currentCupIdx++;
+    Cup[] MakeIndex(int[] cups, int max) {
+      var idx = new Cup[max + 1];
+      var cup = new Cup(cups.First());
+      idx[cup.Value] = cup;
+      foreach(var num in cups.Skip(1)) {
+        idx[num] = new Cup(num);
+        cup.Next = idx[num];
+        cup = idx[num];
       }
-      return cups;
+      cup.Next = idx[cups.First()];
+      return idx;
     }
 
-    long ReadCupsAfterOne(int[] cups) {
-      Func<int,int> mod = (int x) => (x%cups.Length + cups.Length)%cups.Length;
-      var cupsAfterOne = new int[cups.Length -1];
-      var readIdx = Array.IndexOf(cups, 1) + 1;
+
+
+    Cup[] PlayCrabCups(int[] cups, int rounds) {
+      var max = cups.Max();
+      var cupIdx = MakeIndex(cups, max);
+      Func<int, int, int> subtractCup = (int value, int max) => (value - 1) <= 0 ? max : value - 1;
+
+      var cup = cupIdx[cups.First()];
+      for (var i = 1; i <= rounds;i++) {
+        var c1 = cup.Next;
+        var c2 = c1.Next;
+        var c3 = c2.Next;
+        var nextCup = c3.Next;
+        var targetValue = subtractCup(cup.Value, max);
+        while (targetValue == c1.Value || targetValue == c2.Value || targetValue == c3.Value) {
+          targetValue = subtractCup(targetValue, max);
+        }
+        var targetCup = cupIdx[targetValue];
+        c3.Next = targetCup.Next;
+        targetCup.Next = c1;
+        cup.Next = nextCup;
+        cup = nextCup;
+      }
+      return cupIdx;
+    }
+
+    long ReadCupsAfterOne(Cup[] cups) {
+      var cupsAfterOne = new int[cups.Length -2];
+      var cup = cups[1].Next;
       var writeIdx = 0;
-      while(readIdx < (Array.IndexOf(cups, 1) + cups.Length)) {
-        cupsAfterOne[writeIdx] = cups[mod(readIdx)];
-        readIdx++;
+      while (cup.Value != 1) {
+        cupsAfterOne[writeIdx] = cup.Value;
+        cup = cup.Next;
         writeIdx++;
       }
       return Int64.Parse(string.Join("", cupsAfterOne));
+    }
+
+    long ReadNextTwoCups(Cup[] cups) {
+      return (long)cups[1].Next.Value * (long)cups[1].Next.Next.Value;
     }
 
     int[] Parse(string input) {
